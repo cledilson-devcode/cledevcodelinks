643216a93d68a2e52d5248b359c1b936c8fec4c7
cledilson <cledilsondevoffice@gmail.com>
2024-02-23 21:17:43 -0300

    feat: adicionada a alteração da imagem através do onclick

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 function toggleMode() {
-    
     const body = document.body;
-    body.classList.toggle('light');
+    body.classList.toggle("light");
 
+
+    // Substituir imagem
+    const img = document.querySelector("#profile img");
+
+    if (body.classList.contains('light')) {
+        img.setAttribute("src", "./assets/cledilsondevcodewhite.png")
+    }else{
+        img.setAttribute("src", "./assets/cledilsondevcodeblack.png") 
+    }
 }
